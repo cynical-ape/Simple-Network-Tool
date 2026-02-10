49574b15352da5aa1dfd289257a1c27f7566874a
cynical-ape <is.muhammad.sb@gmail.com>
2026-02-10 18:19:57 +0200

    Add files via upload

---
+++ ConnectionTool.ps1
@@ -0,0 +1,34 @@
+ do {
+    Write-Host "`n--- Network Utility Menu ---" -ForegroundColor Cyan
+    Write-Host "1- Check Internet Connection" -ForegroundColor Green
+    Write-Host "2- Check Your IP" -ForegroundColor Yellow
+    Write-Host "3- Flush DNS" -ForegroundColor Blue
+    Write-Host "Q- Quit" -ForegroundColor Red
+
+    
+
+    switch ($choice = Read-Host "Please Insert Your Choice") {
+    "1"{
+        if (Test-Connection -ComputerName 8.8.8.8 -Count 1 -Quiet) {
+         Write-Host "Success: You are connected to the internet." -ForegroundColor Green
+        } else {
+            Write-Host "Error: No internet connection found." -ForegroundColor Red}
+        }
+    "2"{ 
+        $ip = Get-NetIPAddress -AddressFamily IPv4 | Select-Object -ExpandProperty IPAddress
+        Write-Host "Your IP Addresses are: $ip" -ForegroundColor Green
+    } 
+
+    "3"{
+        Clear-DnsClientCache
+        Write-Host "DNS has been cleared successfully." -ForegroundColor Yellow
+    } 
+    "q"{ Write-Host "Closing...`nThank you!" -ForegroundColor DarkCyan
+        break
+    }
+    default {
+        Write-Host "Invalid choice, please try again." -ForegroundColor Red
+    }
+}}
+while ($choice -ne "q")
+
